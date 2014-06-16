aadc3eeab5b76f97416ec75c9f2e6df9d94c4edc
Ivan R. Judson <irjudson@gmail.com>
2014-06-15 18:03:17 -0700

    More config updates.

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://127.0.0.1:2368',
+        url: 'http://irjudson.azurewebsites.net',
         mail: {},
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://irjudson.azurewebsites.com/',
+        url: 'http://irjudson.azurewebsites.com',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -61,7 +61,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: process.env.PORT
+            port: process.env.PORT || 2368
         }
     },
 
